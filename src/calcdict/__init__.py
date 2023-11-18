68b7ba5a58599f66945242dcdaecbfa958e2fbe5
Johannes <johannes-programming@posteo.org>
2023-11-18 11:47:55 +0100

    Version 1.0.1

--- src/calcdict/__init__.py
+++ src/calcdict/__init__.py
@@ -4,6 +4,10 @@ import fileunity as _fu
 class CalcDict:
     def __init__(self, calculators={}, results={}):
         self._busy = set()
+        self._calculators = dict()
+        self._dummy_unit = _fu.TOMLUnit()
+        self._results_unit = _fu.TOMLUnit()
+
         calculators = dict(calculators)
         results = _fu.TOMLUnit(results).data
         self.update_calculators(calculators)
@@ -14,6 +18,7 @@ class CalcDict:
     def get_calculator(self, key):
         return self._calculators[key]
     def set_calculator(self, key, value):
+        self._strerror(key)
         self._dummy_unit[key] = ""
         self._calculators[key] = value
         if key in self._results_unit.keys():
@@ -30,13 +35,16 @@ class CalcDict:
     def results(self):
         return self._results_unit.data
     def get_result(self, key):
+        self._strerror(key)
         return self._results_unit[key]
     def set_result(self, key, value):
+        self._strerror(key)
         self._results_unit[key] = value
         if key in self._dummy_unit.keys():
             del self._dummy_unit[key]
             del self._calculators[key]
     def del_result(self, key):
+        self._strerror(key)
         del self._results_unit[key]
     def update_results(self, dictionary):
         dictionary = dict(dictionary)
@@ -45,26 +53,42 @@ class CalcDict:
     def clear_results(self):
         self._results_unit.clear()
 
-    def getitem(self, firstkey, *otherkeys):
-        calculator = self._calculators.pop(firstkey)
-        del self._dummy_unit[firstkey]
-        if firstkey in self._busy:
-            raise KeyError
-        self._busy.add(firstkey)
+    def getitem(self, key):
         try:
-            self._results_unit[firstkey] = calculator(self.getitem)
+            calculator = self._calculators.pop(key)
+        except KeyError:
+            return self._results_unit[key]
+        del self._dummy_unit[key]
+        if key in self._busy:
+            raise KeyError(f"The item {key} was requested during its own calculation.")
+        self._busy.add(key)
+        try:
+            self._results_unit[key] = calculator(self.getitem)
         finally:
-            self._busy.remove(firstkey)
-        return self._results_unit[firstkey]
+            self._busy.remove(key)
+        return self._results_unit[key]
     def clear(self):
         self.clear_calculators()
         self.clear_results()
+    def calculate_all(self):
+        keys = list(self._calculators.keys())
+        for k in keys:
+            self.getitem(k)
     def copy(self):
         cls = type(self)
         return cls(
             calculators=self.calculators(), 
             results=self.results(),
         )
+    def to_dict(self):
+        self.calculate_all()
+        return self._results_unit.data
+    def to_TOMLUnit(self):
+        return _fu.TOMLUnit(self.to_dict())
+    @classmethod
+    def _strerror(self, key):
+        if type(key) is not str:
+            raise TypeError(f"The key {key} is not a string.")
 
 
     
